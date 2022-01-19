62fc85715d71a6909aa9cd3f6fe16d78ee32a23c
Ramya <ramyapattem1999@gmail.com>
2022-01-19 11:08:05 +0530

    changed variables for naming convention

--- script.js
+++ script.js
@@ -4,78 +4,75 @@ let Name = document.getElementById('name');
 let zipCode = document.getElementById('zipCode');
 let email = document.getElementById('Email');
 let country = document.getElementById('Country');
-
+let empty_string = "";
 function validate(e) {
     e.preventDefault();
     let errors = [];
-    let userIdValue = userId.value;
-    let passwordvalue = password.value;
-    let nameValue = Name.value;
-    let countryName = country.value;
-    let zipCodevalue = zipCode.value;
-    let emailvalue = email.value;
 
     //to validate userId
-    if (userIdValue === "") {
+    if (userId.value === empty_string) {
         document.getElementById('userIdError').innerHTML = "UserId can't be empty";
         userId.classList.add("border", "border-danger");
         errors.push("UserId can't be empty")
-    } else if ((userIdValue.length < 5) || (userIdValue.length > 12)) {
+    } else if ((userId.value.length < 5) || (userId.value.length > 12)) {
         document.getElementById('userIdError').innerHTML = "UserId can have 5 to 12 digits only";
         userId.classList.add("border border-danger");
         errors.push("UserId can have 5 to 12 digits only");
     } else {
         userId.classList.remove("border", "border-danger");
 
-        document.getElementById('userIdError').innerHTML = "";
+        document.getElementById('userIdError').innerHTML = empty_string;
     }
+
+
+    
     //to validate  password
-    if (passwordvalue === "") {
+    if (password.value === empty_string) {
         document.getElementById('pass').innerHTML = "Password can't be empty";
         password.classList.add("border", "border-danger");
         errors.push("Password can't be empty")
-    } else if ((passwordvalue.length < 7) || (passwordvalue.length > 12)) {
+    } else if ((password.value.length < 7) || (password.value.length > 12)) {
         document.getElementById('pass').innerHTML = "Password can have 7 to 12 characters only";
         password.classList.add("border", "border-danger");
         errors.push("Password can have 7 to 12 characters only")
     } else {
-        document.getElementById('pass').innerHTML = "";
+        document.getElementById('pass').innerHTML = empty_string;
         password.classList.remove("border", "border-danger");
     }
 
 
     // to validate name
-    if (nameValue === "") {
+    if (Name.value === empty_string) {
         document.getElementById('username').innerHTML = "Name can't be empty";
         Name.classList.add("border", "border-danger");
         errors.push("Name can't be empty")
-    } else if (!nameValue.match(/^[A-Za-z]+$/)) {
+    } else if (!Name.value.match(/^[A-Za-z]+$/)) {
         document.getElementById('username').innerHTML = "Only alphabets are allowed.";
         Name.classList.add("border", "border-danger");
         errors.push("Only alphabets are allowed.");
     } else {
-        document.getElementById('username').innerHTML = "";
+        document.getElementById('username').innerHTML = empty_string;
         Name.classList.remove("border", "border-danger");
     }
 
 
     // to make country mandatory
-    if (countryName === "") {
+    if (country.value === empty_string) {
         document.getElementById('countryname').innerHTML = "Country can't be empty";
         country.classList.add("border", "border-danger");
         errors.push("Country can't be empty")
     } else {
-        document.getElementById('countryname').innerHTML = "";
+        document.getElementById('countryname').innerHTML = empty_string;
         country.classList.remove("border", "border-danger");
     }
 
 
     // to make  zipcode mandatory
-    if (zipCodevalue === "") {
+    if (zipCode.value === "") {
         document.getElementById('codeNumber').innerHTML = "Zipcode can't be empty";
         zipCode.classList.add("border", "border-danger");
         errors.push("Zipcode can't be empty");
-    } else if ((zipCodevalue.length < 6) || (zipCodevalue.length > 8)) {
+    } else if ((zipCode.value.length < 6) || (zipCode.value.length > 8)) {
         document.getElementById('codeNumber').innerHTML = "Zipcode must have atleast 6 to 8 digits";
         zipCode.classList.add("border", "border-danger");
         errors.push("Zipcode must have atleast 6 to 8 digits");
@@ -88,11 +85,11 @@ function validate(e) {
 
     //to validate email
     let expression = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (emailvalue === "") {
+    if (email.value === "") {
         document.getElementById('emailId').innerHTML = "Email can't be empty";
         email.classList.add("border", "border-danger");
         errors.push("Email can't be empty")
-    } else if (!emailvalue.match(expression)) {
+    } else if (!email.value.match(expression)) {
         document.getElementById('emailId').innerHTML = "Invalid email id";
         email.classList.add("border", "border-danger");
         errors.push("Invalid email id")
@@ -103,64 +100,64 @@ function validate(e) {
 
 
     // to validate gender
-    let radio1 = document.getElementById('radio1');
-    let radio2 = document.getElementById('radio2');
+    let male = document.getElementById('radio1');
+    let female = document.getElementById('radio2');
     let genderValue = "";
-    if ((radio1.checked === false) && (radio2.checked === false)) {
+    if ((male.checked === false) && (female.checked === false)) {
         document.getElementById('genderId').innerHTML = " gender is required";
-        radio1.classList.add("border", "border-danger");
-        radio2.classList.add("border", "border-danger");
+        male.classList.add("border", "border-danger");
+        female.classList.add("border", "border-danger");
         errors.push("gender is required")
     } else {
-        if (radio1.checked) {
-            genderValue = radio1.value;
-        } else if (radio2.checked) {
-            genderValue = radio2.value;
+        if (male.checked) {
+            genderValue = male.value;
+        } else if (female.checked) {
+            genderValue = female.value;
         }
         document.getElementById('genderId').innerHTML = "";
-        radio1.classList.remove("border", "border-danger");
-        radio2.classList.remove("border", "border-danger");
+        male.classList.remove("border", "border-danger");
+        female.classList.remove("border", "border-danger");
     }
 
 
     //to make language mandatory
-    let checkbox1 = document.getElementById('checkbox1');
-    let checkbox2 = document.getElementById('checkbox2');
+    let english = document.getElementById('checkbox1');
+    let nonEnglish = document.getElementById('checkbox2');
     let languageValue = "";
-    if ((checkbox1.checked === false) && (checkbox2.checked === false)) {
+    if ((english.checked === false) && (nonEnglish.checked === false)) {
         document.getElementById('language').innerHTML = " Language is required";
-        checkbox1.classList.add("border", "border-danger");
-        checkbox2.classList.add("border", "border-danger");
+        english.classList.add("border", "border-danger");
+        nonEnglish.classList.add("border", "border-danger");
         errors.push("Language is required");
     } else {
-        if (checkbox1.checked) {
-            languageValue = checkbox1.value;
-            if (checkbox2.checked) {
-                languageValue += " , " + checkbox2.value;
+        if (english.checked) {
+            languageValue = english.value;
+            if (nonEnglish.checked) {
+                languageValue += " , " + nonEnglish.value;
             }
-        } else if (checkbox2.checked) {
-            languageValue = checkbox2.value;
+        } else if (nonEnglish.checked) {
+            languageValue = nonEnglish.value;
         }
         document.getElementById('language').innerHTML = "";
-        checkbox1.classList.remove("border", "border-danger");
-        checkbox2.classList.remove("border", "border-danger");
+        english.classList.remove("border", "border-danger");
+        nonEnglish.classList.remove("border", "border-danger");
     }
     console.log(errors)
 
 
-// to store data in localStorage
+    // to store data in localStorage
     if (errors.length === 0) {
         let data = localStorage.getItem("user_data") ? JSON.parse(localStorage.getItem("user_data")) : [];
         data.push({
-            "userId": userIdValue,
-            "name": nameValue,
-            "country": countryName,
-            "zipcode": zipCodevalue,
-            "email": emailvalue,
+            "userId": userId.value,
+            "name": Name.value,
+            "country": country.value,
+            "zipcode": zipCode.value,
+            "email": email.value,
             "gender": genderValue,
             "language": languageValue,
             "about": document.getElementById("aboutId").value,
-            "password": passwordvalue,
+            "password": password.value,
             "address": document.getElementById("addressId").value,
         });
         console.log(data);
